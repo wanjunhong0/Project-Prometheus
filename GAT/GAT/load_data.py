9f410a5174d130a0a7a0bbebbb1f920e5524e208
unknown <wanjunhong@hikvision.com>
2020-11-05 20:38:39 +0800

    implement mini-batch feature

--- GAT/GAT/load_data.py
+++ GAT/GAT/load_data.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy.sparse as sp
 import pandas as pd
 import torch
 import torch.nn.functional as F
@@ -8,13 +7,12 @@ import argparse
 
 
 class Data(object):
-    def __init__(self, path, adj_type, test_size, seed):
+    def __init__(self, path, test_size, seed):
         """Load dataset
            Preprocess feature, label, normalized adjacency matrix and train/val/test index
            
         Args:
             path (str): file path
-            adj_type (str): the type of laplacian matrix of adjacency matrix {'single', 'double'}
             test_size (float): the testset proportion of the dataset 
             seed (int): random seed
         """
@@ -26,6 +24,7 @@ class Data(object):
         df[0] = df[0].map(id_map)
         edge_list[0] = edge_list[0].map(id_map)
         edge_list[1] = edge_list[1].map(id_map)
+        edge_list = torch.LongTensor(edge_list.values)
         # feature, label
         df = df.set_index(0)
         self.feature = torch.FloatTensor(df.iloc[:, :-1].values)
@@ -36,13 +35,43 @@ class Data(object):
         # graph
         self.n_node = df.shape[0]
         self.n_edge = edge_list.shape[0]
-        adj = sp.coo_matrix((np.ones(self.n_edge), (edge_list[0], edge_list[1])), shape=(self.n_node, self.n_node),dtype=np.float32)
+        adj = torch.sparse.FloatTensor(edge_list.T, torch.ones(self.n_edge), torch.Size([self.n_node, self.n_node]))
         # build symmetric adjacency matrix
-        adj = adj + adj.T.multiply(adj.T > adj) + sp.eye(adj.shape[0])
-        self.edge_list = torch.from_numpy(np.vstack((adj.tocoo().row, adj.tocoo().col)).astype(np.int64))
+        adj = torch.add(torch.eye(self.n_node).to_sparse(), torch.add(adj, adj.transpose(0, 1)))
+        self.edge_list = adj._indices()
         # train, val, test split
         self.idx_train, self.idx_test = train_test_split(range(self.n_node), test_size=test_size, random_state=seed)
         self.idx_train, self.idx_val = train_test_split(self.idx_train, test_size=test_size, random_state=seed)
         self.idx_train = torch.LongTensor(self.idx_train)
         self.idx_val = torch.LongTensor(self.idx_val)
         self.idx_test = torch.LongTensor(self.idx_test)
+
+
+class Dataset(torch.utils.data.Dataset):
+    """ Generate train, val, test dataset for the model """
+    def __init__(self, edge_list, idx):
+        self.edge_list = edge_list
+        self.idx = idx
+
+    def __len__(self):
+        return len(self.idx)
+
+    def __getitem__(self, i):
+        """ Retrieve node_id and edge list of its 2-hop neighbor """
+        idx = self.idx[i]
+        # two layers of graph conv need edge list of 2-hop neighbors
+        neighbor = self.edge_list[:, self.edge_list[0] == idx][1]
+        edge_list  = self.edge_list[:, (self.edge_list[0].view(-1, 1) == neighbor).any(-1)]
+
+        return edge_list, idx
+
+
+def collate(batch):
+    """ Collate function for mini-batch, can't use default collate_fn due to edge_list in different size"""
+    edge_list = torch.cat([i for i, _ in batch], dim=1)
+    idx = torch.LongTensor([j for _, j in batch])
+
+    return edge_list, idx
+
+
+
--- GAT/main.py
+++ GAT/main.py
@@ -4,17 +4,20 @@ import random
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import DataLoader
 from sklearn.metrics import accuracy_score
 
 from GAT.parser import parse_args
 from GAT.models import GAT
-from GAT.load_data import Data
+from GAT.load_data import Data, Dataset, collate
 
 # Settings
 args = parse_args()
+args = parse_args()
+for arg in vars(args):
+    print('{0} = {1}'.format(arg, getattr(args, arg)))
 torch.manual_seed(args.seed)
 random.seed(args.seed)
-
 # training on the first GPU if not on CPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print('Training on device = {}'.format(device))
@@ -24,16 +27,16 @@ print('Training on device = {}'.format(device))
 Loading data
 ===========================================================================
 """
-data = Data(path=args.data_path + args.dataset, adj_type=args.adj_type, test_size=args.test_size, seed=args.seed)
+data = Data(path=args.data_path + args.dataset, test_size=args.test_size, seed=args.seed)
 print('Loaded {0} dataset with {1} nodes and {2} edges'.format(args.dataset, data.n_node, data.n_edge))
 feature = data.feature.to(device)
 label = data.label
-# adj = data.adj
-idx_train = data.idx_train
-idx_val = data.idx_val
-idx_test = data.idx_test
-edge_list = data.edge_list.to(device)
-
+train = Dataset(data.edge_list, data.idx_train)
+val = Dataset(data.edge_list, data.idx_val)
+test = Dataset(data.edge_list, data.idx_test)
+train_loader = DataLoader(dataset=train, batch_size=args.batch_size, collate_fn=collate)
+val_loader = DataLoader(dataset=val, batch_size=args.batch_size, collate_fn=collate)
+test_loader = DataLoader(dataset=test, batch_size=args.batch_size, collate_fn=collate)
 
 """
 ===========================================================================
@@ -49,21 +52,28 @@ for epoch in range(1, args.epoch+1):
     t = time.time()
     # Training
     model.train()
-    optimizer.zero_grad()
-    output = model(feature, edge_list).cpu()
-    loss_train = F.nll_loss(input=output[idx_train], target=label[idx_train])
-    acc_train = accuracy_score(y_pred=output[idx_train].max(1)[1], y_true=label[idx_train])
-    loss_train.backward()
-    optimizer.step()
+    loss_train, acc_train = 0, 0
+    for edge_list_batch, idx_batch in train_loader:
+        optimizer.zero_grad()
+        output = model(feature, edge_list_batch.to(device)).cpu()
+        loss = F.nll_loss(input=output[idx_batch], target=label[idx_batch])  # mean loss to backward
+        loss.backward()
+        optimizer.step()
+        loss_train += loss.item() * len(idx_batch)
+        acc_train += accuracy_score(y_pred=output[idx_batch].max(1)[1], y_true=label[idx_batch], normalize=False)
+    loss_train, acc_train = [x / len(train) for x in [loss_train, acc_train]]
 
     # Validation
     model.eval()
-    output = model(feature, edge_list).cpu()
-    loss_val = F.nll_loss(input=output[idx_val], target=label[idx_val])
-    acc_val = accuracy_score(y_pred=output[idx_val].max(1)[1], y_true=label[idx_val])
+    loss_val, acc_val = 0, 0
+    for edge_list_batch, idx_batch in val_loader:
+        output = model(feature, edge_list_batch.to(device)).cpu()
+        loss_val += F.nll_loss(input=output[idx_batch], target=label[idx_batch], reduction='sum')
+        acc_val += accuracy_score(y_pred=output[idx_batch].max(1)[1], y_true=label[idx_batch], normalize=False)
+    loss_val, acc_val = [x / len(val) for x in [loss_val, acc_val]]
 
     print('Epoch {0:04d} | time: {1:.2f}s | Loss = [train: {2:.4f}, val: {3:.4f}] | ACC = [train: {4:.4f}, val: {5:.4f}]'
-          .format(epoch, time.time() - t, loss_train.item(), loss_val.item(), acc_train, acc_val))
+          .format(epoch, time.time() - t, loss_train, loss_val, acc_train, acc_val))
 
 """
 ===========================================================================
@@ -71,8 +81,12 @@ Testing
 ===========================================================================
 """
 model.eval()
-output = model(feature, edge_list).cpu()
-loss_test = F.nll_loss(input=output[idx_test], target=label[idx_test])
-acc_test = accuracy_score(y_pred=output[idx_test].max(1)[1], y_true=label[idx_test])
+loss_test, acc_test = 0, 0
+for edge_list_batch, idx_batch in test_loader:
+    output = model(feature, edge_list_batch.to(device)).cpu()
+    loss_test += F.nll_loss(input=output[idx_batch], target=label[idx_batch], reduction='sum')
+    acc_test += accuracy_score(y_pred=output[idx_batch].max(1)[1], y_true=label[idx_batch], normalize=False)
+loss_test, acc_test = [x / len(test) for x in [loss_test, acc_test]]
+
 print('======================Testing======================')
-print('Loss = [test: {0:.4f}] | ACC = [test: {1:.4f}]'.format(loss_test.item(), acc_test))
+print('Loss = [test: {0:.4f}] | ACC = [test: {1:.4f}]'.format(loss_test, acc_test))
